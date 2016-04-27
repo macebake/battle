37ecb83a61b272fb9e867463ae347647bd1ef00b
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-04-27 12:28:00 +0100

    second feature passing

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -8,3 +8,12 @@ feature 'Enter names' do
     expect(page).to have_content 'Harry vs. Mara'
   end
 end
+feature 'see other players hit points' do
+  scenario 'see how close to winning' do
+    visit ('/')
+    fill_in :player_1_name, with: 'Harry'
+    fill_in :player_2_name, with: 'Mara'
+    click_button 'Submit'
+    expect(page).to have_content 'Mara: 60HP'
+  end
+end
