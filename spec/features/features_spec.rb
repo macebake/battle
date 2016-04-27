40a2ac5ed3f78cc282e073b780350fb95069e5ce
Mara Wanot <m.d.wanot@hotmail.com>
2016-04-27 14:14:19 +0100

    refactored :D and added helper method

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,19 +1,15 @@
 require 'spec_helper'
+
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit ('/')
-    fill_in :player_1_name, with: 'Harry'
-    fill_in :player_2_name, with: 'Mara'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Harry vs. Mara'
   end
 end
+
 feature 'see other players hit points' do
   scenario 'see how close to winning' do
-    visit ('/')
-    fill_in :player_1_name, with: 'Harry'
-    fill_in :player_2_name, with: 'Mara'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Mara: 60HP'
   end
 end
