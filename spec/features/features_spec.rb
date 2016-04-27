871051e774eb96660f078e1c4322f5b6efc489f7
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-04-27 15:22:23 +0100

    attack with rspecs added

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -13,3 +13,11 @@ feature 'see other players hit points' do
     expect(page).to have_content 'Mara: 60HP'
   end
 end
+
+feature 'Attacking' do
+  scenario 'Attack player 2' do
+    sign_in_and_play
+    click_button 'Attack'
+    expect(page).to have_content 'Harry attacked Mara'
+  end
+end
